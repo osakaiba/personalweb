22238d38b227148dc0db285a3c72517af555fa3f
osakaiba <osakaiba@gmail.com>
2024-03-13 23:53:55 -0500

    uso de lectura de json en línea

---
+++ src/components/HelloComponent.jsx
@@ -0,0 +1,20 @@
+function HelloComponent(){
+  
+  return (
+  <div>
+    <h1>
+      React Hello  
+    </h1>
+   
+    <button onClick= {()=>{
+
+      alert("hola pelado")
+    } }>
+      clich me
+    </button>
+
+  </div>
+  )
+}
+
+export default HelloComponent
